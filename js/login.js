571ffdd601eb512a309d1ceed69ac36d94bd83c1
Sqll <m15806988287@163.com>
2022-05-13 19:15:54 +0800

    5.13

--- js/login.js
+++ js/login.js
@@ -4,12 +4,12 @@ $('#login-button').click(function (event) {
     //修改密码请改此处
     if(userName=="张大牙" &&  pwd=="19980513"){
         event.preventDefault();
-        $('login').fadeOut(500);
-        $('.big-box').addClass('form-success');
+        //$('login').fadeOut(500);
+        //$('.big-box').addClass('form-success');
         //requestFullScreen();  全屏
         setTimeout(function(){
             location.href="index1.html";
-        },2000);
+        },500);
     }
     else{
         alert("Wrong Password");
