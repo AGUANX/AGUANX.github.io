ece49a6b4b973a63229f220ad7178f4c4c7e4d98
Sqll <m15806988287@163.com>
2022-08-08 20:47:05 +0800

    8.8

--- js/where.js
+++ js/where.js
@@ -1,16 +1,25 @@
-const panels = document.querySelectorAll('.panel');
-
-panels.forEach(panel => {
-    //console.log(111);
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
-
-    })
-})
-
 function removeActiveClasses() {
+    const panels = document.querySelectorAll('.panel');
     panels.forEach(panel => {
         panel.classList.remove('active');
     })
 }
+
+function active(panel){
+    removeActiveClasses();
+    panel.classList.add('active');
+}
+
+const sky = document.getElementById('sky');
+sky.addEventListener('click',function (){
+    setTimeout(function(){
+        location.href="skystar.html";
+    },500);
+})
+
+const cat = document.getElementById('cat');
+cat.addEventListener('click',function (){
+    setTimeout(function(){
+        location.href="cat/第一节/第一节.html";
+    },500);
+})
